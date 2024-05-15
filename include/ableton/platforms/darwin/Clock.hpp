9ad7bb41e0daf5012c3bfd3650766df66aff3145
Hamilton <pwnified@harmonicdog.com>
2024-05-14 17:48:19 -0700

    Negative Ticks again

--- include/ableton/platforms/darwin/Clock.hpp
+++ include/ableton/platforms/darwin/Clock.hpp
@@ -51,7 +51,7 @@ struct Clock
   Ticks microsToTicks(const Micros micros) const
   {
     // Negative Micros can not be represented in Ticks
-    assert(micros.count() >= 0);
+    // assert(micros.count() >= 0);
     return static_cast<Ticks>(micros.count() / mTicksToMicros);
   }
 
